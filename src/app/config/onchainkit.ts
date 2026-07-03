84e750e3a40ccec0821c8e999a777829f503b54e
Akhmad Khudri <khudri@binadarma.ac.id>
2026-07-03 11:57:34 +0700

    Delete src directory

--- src/app/config/onchainkit.ts
+++
@@ -1,4 +0,0 @@
-// OnchainKit API Configuration
-// Note: If you get 401 errors, you may need to get a new API key from https://portal.cdp.coinbase.com/
-export const ONCHAINKIT_API_KEY = process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY || 'ockKey-0s97nv7i9u';
-export const ONCHAINKIT_PROJECT_ID = process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_ID || 'e49aa08c-0cb0-406d-96dd-ba8adda4efe1';
